0c84f94bc80606310053c32f245c93f02654b5d0
mayalexandera <maya.blevins@gmail.com>
2021-01-09 18:59:18 -0500

    create remove records tests

--- test/reading_test.js
+++ test/reading_test.js
@@ -2,6 +2,7 @@ const assert = require("assert");
 const User = require("../src/user");
 
 describe("reading users out of the database", () => {
+  let joe
   beforeEach((done) => {
     joe = new User({ name: "Joe" });
     joe.save().then(() => done());
@@ -14,7 +15,16 @@ describe("reading users out of the database", () => {
       done();
     });
   });
+
+  it("finds a user with a particular id", (done) => {
+    User.findOne({ _id: joe.id }).then((user) => {
+      assert(user.name === "Joe")
+      done();
+    });
+  });
 });
+
+
 
 /**
  * User.find(criteria) = find all the users that match the given criteria.
