b9aff490eae02a5393d9ff5b0df8332abf1385d5
mayalexandera <maya.blevins@gmail.com>
2021-01-09 22:53:07 -0500

    create update subdocument tests

--- src/user.js
+++ src/user.js
@@ -1,11 +1,24 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const PostSchema = require("./post");
 
 const UserSchema = new Schema({
-  name: String,
-  postCount: Number,
+  name: {
+    type: String,
+    required: [true, "Given name is required."],
+    validate: {
+      validator: (name) => name.length > 2,
+      message: "Given name must be longer than 2 characters.",
+    },
+  },
+  posts: [PostSchema],
+  likes: Number
 });
 
-const User = mongoose.model('user', UserSchema)
+UserSchema.virtual("postCount").get(function () {
+  return this.posts.length
+});
+
+const User = mongoose.model("User", UserSchema);
 
 module.exports = User;
--- test/update_test.js
+++ test/update_test.js
@@ -24,21 +24,22 @@ describe("updating records", () => {
   });
 
   it("a model instance can update", (done) => {
-    assertName(joe.updateOne({ name: "Alex" }), done);
+    assertName(joe.updateOne({ name: "Alex", likes: 0 }), done);
   });
 
-  it('a model can find and update a record by id', (done) => {
-    assertName(User.findByIdAndUpdate(joe._id, { name: "Alex"}), done)
-  })
+  it("a model can find and update a record by id", (done) => {
+    assertName(User.findByIdAndUpdate(joe._id, { name: "Alex", likes: 0 }), done);
+  });
 
-  it('a model can update postCount by 1', (done) => {
-    User.updateOne({ name: "Joe" }, { $inc: { postCount: 1 } })
-    .then(() => User.findOne({ name: "Joe" }))
-    .then((user) => {
-      assert(user.postCount === 1 )
-      done()
-    })
-  })
+  it("a model can update postCount by 1", (done) => {
+    User.updateOne({ name: "Joe" }, { $inc: { likes: 1 } })
+      .then(() => User.findOne({ name: "Joe" }))
+      .then((user) => {
+        console.log(user.likes)
+        assert(user.likes === 1);
+        done();
+      });
+  });
 });
 /**
  * There are many ways to Update a record in db.
